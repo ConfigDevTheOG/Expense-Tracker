ee9c6ecdb5234a67adaa59e326ecf49e83cb8922
LÖÖPS <lolzggzez@gmail.com>
2026-06-17 18:25:44 +0600

    Added CLI based option chooser

--- Expense Tracker/main.py
+++ Expense Tracker/main.py
@@ -19,15 +19,35 @@ budget_database = {
     "monthly_balance": 0
 }
 
-data+
+database = {
+   "income": 0,
+    "rent": 0,
+    "groceries": 0,
+    "electricity": 0,
+    "education": 0,
+    "basic_needs": 0,
+    "entertainment": 0,
+    "child_needs": 0,
+    "extra_expenses": 0,
+    "shopping": 0,
+    "healthcare": 0,
+    "insurance": 0,
+    "total_expenses": 0,
+    "monthly_balance": 0
+}
 
+def main():
+   print("Starting Tracker...")
+
+if __name__ == "__main__":
+   main()
 
 def budget():
     for i in budget_database.keys():
      if i not in ["income", "total_expenses", "monthly_balance"]:
             while True:
                try:
-                  avg_budget = float(input(f"Enter estimated monthly budget of {i}: "))
+                  avg_budget = float(input(f"\nEnter estimated monthly budget of {i}: "))
                   budget_database[i] = avg_budget
 
                except ValueError:
@@ -44,7 +64,7 @@ def budget():
 
      elif i == "total_expenses": 
 
-      
+
         total = 0 
         for k, v in budget_database.items():
            if k not in ["income", "total_expenses", "monthly_balance"]:
@@ -57,8 +77,28 @@ def budget():
 
     with open("E:\\Programming\\Python\\Projects\\Active\\Expense Tracker\\budget.json", "w") as file:
        js.dump({"budget": budget_database}, file, indent=4)
-        
-budget()
 
-for k, v in budget_database.items():
-   print(f"{k}:{v}")
+def expenses():
+   month = input("Enter a month to which you want to add ") 
+
+while True:
+   try:
+      operation = int(input("""
+                            
+      1. Enter monthly budget of the following year
+      2. Enter expenses of a month
+      3. View expenses
+      4. Exit
+                           
+      Please enter the number corresponded with the desired action: """))
+
+      if operation == 4:
+         break
+
+      elif operation == 1:
+         budget()
+
+   except ValueError:
+      print("\nPlease enter a number Please enter the number corresponded with the desired action")
+
+   
